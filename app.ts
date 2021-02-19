27c161bc35501d012bbf5784ce452999886d14d2
developerpalak <developerpalakpatel@gmail.com>
2021-02-19 18:29:50 +0530

    Another Changes

--- app.ts
+++ app.ts
@@ -3,6 +3,8 @@ console.log(msg);
 class Starting {
   start(): void {
     console.log("new code");
+    console.log("line code");
+    console.log("added code");
   }
 }
 var myStart = new Starting();
